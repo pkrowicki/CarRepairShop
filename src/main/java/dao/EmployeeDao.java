abee1cad1f59bf6e7911694caccb3de898ae1155
Pawel Krowicki <pkrowicki@outlook.com>
2017-07-05 14:17:59 +0200

    # Implemented EmployeeDao and EmployeeDaoImpl # Small bugfix in CustomerDaoImpl

--- src/main/java/dao/EmployeeDao.java
+++ src/main/java/dao/EmployeeDao.java
@@ -1,5 +1,6 @@
 package dao;
 
+import model.Customer;
 import model.Employee;
 
 import java.util.List;
@@ -8,10 +9,10 @@ import java.util.List;
  * Created by Pawel Krowicki on 2017-07-05.
  */
 public interface EmployeeDao {
-    void addEmployee();
-    Employee viewEmployee(int id);
+    void addEmployee(Employee employee);
+    Employee viewEmployeeById(int id);
     List<Employee> viewAllEmployees();
-    void editEmployee(int id);
+    void editEmployee(Employee employee, int id);
     void deleteEmployee(int id);
 
 }
--- src/main/java/dao/EmployeeDaoImpl.java
+++ src/main/java/dao/EmployeeDaoImpl.java
@@ -1,7 +1,12 @@
 package dao;
 
 import model.Employee;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
 /**
@@ -10,28 +15,45 @@ import java.util.List;
 public class EmployeeDaoImpl implements EmployeeDao {
 
 
-    @Override
-    public void addEmployee() {
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
 
+    @Override
+    public void addEmployee(Employee employee) {
+        jdbcTemplate.update("INSERT INTO employees(name, surname, number) VALUES (?,?,?)", employee.getEmployeeName(), employee.getEmployeeSurname(), employee.getEmployeeNumber());
     }
 
     @Override
-    public Employee viewEmployee(int id) {
-        return null;
+    public Employee viewEmployeeById(int id) {
+        return jdbcTemplate.queryForObject("SELECT * FROM employees WHERE id = ?", getEmployeeRowMapper(),id);
     }
 
     @Override
     public List<Employee> viewAllEmployees() {
-        return null;
+        return jdbcTemplate.query("SELECT * FROM employees", getEmployeeRowMapper());
     }
 
     @Override
-    public void editEmployee(int id) {
-
+    public void editEmployee(Employee employee, int id) {
+        jdbcTemplate.update("UPDATE employees SET cemployeename=?, employeesurname=?,employeenumber=? WHERE id = ?", employee.getEmployeeName(), employee.getEmployeeSurname(), employee.getEmployeeNumber(),id);
     }
 
     @Override
     public void deleteEmployee(int id) {
+        jdbcTemplate.update("DELETE FROM employees WHERE id=?", id);
+    }
 
+    private RowMapper<Employee> getEmployeeRowMapper(){
+        return new RowMapper<Employee>() {
+            @Override
+            public Employee mapRow(ResultSet resultSet, int i) throws SQLException {
+                int id = resultSet.getInt("id");
+                String employeename = resultSet.getString("employeename");
+                String employeesurname = resultSet.getString("employeesurname");
+                String employeenumber = resultSet.getString("employeenumber");
+
+                return new Employee(id,employeename,employeesurname,employeenumber);
+            }
+        };
     }
 }
